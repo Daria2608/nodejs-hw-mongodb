af646cd8549c7f30c0ac643a7f1f1a6f7646fe82
Daria2608 <dariya.pitra@gmail.com>
2024-06-26 12:54:33 +0300

    init-2

---
+++ src/server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+
+
+export const setupServer = () => {
+
+const app = express();
+
+const PORT = 3000;
+
+const logger = pino({
+        transport: {
+            target: "pino-pretty"
+        }
+    });
+
+app.use(logger);
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    console.log('1');
+    next();
+});
+
+app.get('/contacts', (request, response) => {
+    // response.send(contacts);
+    response.json({
+    message: 'Hello world!',
+  });
+});
+
+    app.get('/contacts/:contactId', (request, response) => {
+     response.json({
+    message: 'Hello world!',
+  });
+ });
+
+app.use((request, response) => {
+    response.status(404).json({
+  message: 'Not found',
+}
+);
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+};
+
+
+
+ 
+
